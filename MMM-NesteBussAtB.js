ae52b389e7c2406aebf58dd36888f91815fc558a
Otto Paulsen <otto.paulsen@itema.no>
2018-10-08 21:29:21 +0200

    Show times stacked

--- MMM-NesteBussAtB.js
+++ MMM-NesteBussAtB.js
@@ -1,5 +1,5 @@
 
-Module.register("MMM-NesteBussAtB",{
+Module.register("MMM-NesteBussAtB", {
     // Default module config
     defaults: {
         showIcon: true,
@@ -10,94 +10,147 @@ Module.register("MMM-NesteBussAtB",{
         size: "medium",
         stopIds: [16011496, 16010496],
         maxCount: 2, // Max number of next buses per route
-        maxMinutes: 45 // Do not show buses more then this minutes into the future
+        maxMinutes: 45, // Do not show buses more then this minutes into the future
+        stacked: true // Show multiple buses on same row, if same route and destination
     },
 
-	start: function() {
+    start: function () {
         console.log(this.name + ' started.')
-		this.buses = [];
-		this.openBusConnection();
+        this.buses = [];
+        this.openBusConnection();
         var self = this;
-        setInterval(function() {
-            self.updateDom(1000);
+        setInterval(function () {
+            self.updateDom(0);
         }, 10000);
-	},
+    },
 
-	openBusConnection: function() { 
-		this.sendSocketNotification('NESTEBUSSATB_CONFIG', this.config);
-	},
+    openBusConnection: function () {
+        console.log('Sending NESTEBUSSATB_CONFIG with config: ', this.config);
+        this.sendSocketNotification('NESTEBUSSATB_CONFIG', this.config);
+    },
 
-	socketNotificationReceived: function(notification, payload) {
-		if(notification === 'BUS_DATA'){
-			if(payload != null) {
-				this.buses = payload;
-                var count = this.buses.length;
-				this.updateDom();
-			} else {
+    socketNotificationReceived: function (notification, payload) {
+        if (notification == 'BUS_DATA') {
+            if (payload != null) {
+                this.buses = this.config.stacked ? this.stackBuses(payload) : payload;
+                this.updateDom();
+            } else {
                 console.log(this.name + ': BUS_DATA - No payload');
             }
-		}
-	},
+        }
+    },
 
-    getStyles: function() {
+    stackBuses: function (buses) {
+        stackedBuses = [];
+
+        buses.sort(function (a, b) {
+            // return (self.toDate(a.time) - self.toDate(b.time));
+            return ('' + a.from + a.number + a.to + a.time).localeCompare('' + b.from + b.number + b.to + b.time);
+        });
+
+        var len = buses.length;
+        var previousStackvalue = '';
+        var stackedTimes = [];
+        if (len > 0) {
+            previousStackvalue = '' + buses[0].from + buses[0].number + buses[0].to;
+            stackedTimes.push(buses[0].time);
+            for (var i = 1; i < len; i++) {
+                stackvalue = '' + buses[i].from + buses[i].number + buses[i].to;
+                if (stackvalue == previousStackvalue) {
+                    stackedTimes.push(buses[i].time);
+                } else {
+                    stackedBuses.push({
+                        from: buses[i - 1].from,
+                        number: buses[i - 1].number,
+                        to: buses[i - 1].to,
+                        times: stackedTimes
+                    });
+                    previousStackvalue = stackvalue;
+                    stackedTimes = [];
+                    stackedTimes.push(buses[i].time)
+                }
+            }
+            stackedBuses.push({
+                from: buses[len - 1].from,
+                number: buses[len - 1].number,
+                to: buses[len - 1].to,
+                times: stackedTimes
+            });
+        }
+        return stackedBuses;
+    },
+
+    getStyles: function () {
         return [
             'NesteBussAtB.css'
         ];
     },
 
-    getDom: function() {
+    getDom: function () {
         self = this;
-		var wrapper = document.createElement("table");
-		wrapper.className = "medium";
+        var wrapper = document.createElement("table");
+        wrapper.className = "medium";
         var first = true;
 
-		if (self.buses.length === 0) {
-			wrapper.innerHTML = (self.loaded) ? self.translate("EMPTY") : self.translate("LOADING");
-			wrapper.className = "medium dimmed";
+        if (self.buses.length === 0) {
+            wrapper.innerHTML = (self.loaded) ? self.translate("EMPTY") : self.translate("LOADING");
+            wrapper.className = "medium dimmed";
             console.log(self.name + ': No buses');
-			return wrapper;
-		}        
-        
-        self.buses.forEach(function(bus){
-            var busTime = self.toDate(bus.time);
-            var now = new Date();
-            var minutes = Math.round((busTime - now) / 60000);
+            return wrapper;
+        }
 
-			var busWrapper = document.createElement("tr");
-			busWrapper.className = 'border_bottom ' + self.config.size + (first ? ' border_top' : '');
+        self.buses.forEach(function (bus) {
+            var now = new Date();
+            var minutes = '';
+            if(self.config.stacked) {
+                if(bus.times.length > 0) {
+                    var busTime = self.toDate(bus.times[0]);
+                    minutes = Math.round((busTime - now) / 60000);
+                }
+                for(var i=1; i < bus.times.length; i++){
+                    var busTime = self.toDate(bus.times[i]);
+                    minutes += ', ' + Math.round((busTime - now) / 60000);
+                }
+            } else {
+                var busTime = self.toDate(bus.time);
+                minutes = Math.round((busTime - now) / 60000);
+            }
+
+            var busWrapper = document.createElement("tr");
+            busWrapper.className = 'border_bottom ' + self.config.size + (first ? ' border_top' : '');
             first = false; // Top border only on the first row
 
             // Icon
-			if (self.config.showIcon) {
-				var iconWrapper = document.createElement("td");
+            if (self.config.showIcon) {
+                var iconWrapper = document.createElement("td");
                 iconWrapper.innerHTML = '<i class="fa fa-bus" aria-hidden="true"></i>';
                 iconWrapper.className = "align-right";
                 busWrapper.appendChild(iconWrapper);
-			}
+            }
 
             // Rute
-			if (self.config.showNumber) {
-				var numberWrapper = document.createElement("td");
+            if (self.config.showNumber) {
+                var numberWrapper = document.createElement("td");
                 numberWrapper.innerHTML = bus.number;
                 numberWrapper.className = "atb-number";
                 busWrapper.appendChild(numberWrapper);
-			}
+            }
 
             // Holdeplass
-			if (self.config.showFrom) {
-				var fromWrapper = document.createElement("td");
+            if (self.config.showFrom) {
+                var fromWrapper = document.createElement("td");
                 fromWrapper.innerHTML = bus.from;
                 fromWrapper.className = "align-left atb-from";
                 busWrapper.appendChild(fromWrapper);
-			}
+            }
 
             // Destinasjon
-			if (self.config.showTo) {
-				var toWrapper = document.createElement("td");
+            if (self.config.showTo) {
+                var toWrapper = document.createElement("td");
                 toWrapper.className = "align-left atb-to";
                 toWrapper.innerHTML = bus.to;
                 busWrapper.appendChild(toWrapper);
-			}
+            }
 
             // Minutter
             var minutesWrapper = document.createElement("td");
@@ -106,19 +159,19 @@ Module.register("MMM-NesteBussAtB",{
             busWrapper.appendChild(minutesWrapper);
 
             // Min (text)
-			if (self.config.showMin) {
-				var minWrapper = document.createElement("td");
+            if (self.config.showMin) {
+                var minWrapper = document.createElement("td");
                 minWrapper.className = "align-left";
                 minWrapper.innerHTML = '&nbsp;min';
                 busWrapper.appendChild(minWrapper);
-			}
+            }
 
             wrapper.appendChild(busWrapper);
         });
         return wrapper;
     },
 
-    toDate: function(s){
+    toDate: function (s) {
         // Translate the API date to Date object
         year = s.substring(0, 4);
         month = parseInt(s.substring(5, 7)) - 1;
--- node_helper.js
+++ node_helper.js
@@ -1,102 +1,95 @@
 var NodeHelper = require("node_helper");
 var request = require('request');
 var convert = require('xml-js');
-var buses = [];
-var requestCount = 0;
 
 module.exports = NodeHelper.create({
 
-	start: function() {
-		console.log(this.name + ': Starting node helper');
-        this.loaded = false;
-        var self = this;
-        setInterval(function() {
-            self.readBuses();
-            setTimeout(function(){
-                self.broadcastMessage();
-            }, 1000);
-        }, 30000);	
+    start: function () {
+        console.log(this.name + ': Starting node helper');
     },
 
-	socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: function (notification, payload) {
+        console.log(this.name + ': Received socket notification ' + notification);
         var self = this;
-		if (notification === 'NESTEBUSSATB_CONFIG') {
-            self.config = payload;
+        var buses = new Map();
+        if (notification == 'NESTEBUSSATB_CONFIG') {
             console.log(self.name + ': AtB Connection started');
 
-            // Read it immediately once
-            self.loaded = true;
-            self.readBuses();
-            setTimeout(function(){
-                self.broadcastMessage();
-            }, 1000);
-		}
-	},
-
-    readBuses: function() {
-        if (this.loaded){
-            var self = this;
-            stops = self.config.stopIds;
-            if (!requestCount) buses = [];
-            requestCount = stops.length;
-            stops.forEach(function(stopId){
-                self.getAtbStopTimes(stopId, function(error, data){
-                    if(!error){
-                        var routes = new Map();
-                        for(i =0; i < data.buses.length; i++){
-                            var bus = data.buses[i];
-                            var key = bus.line.trim() + bus.name.trim();
-                            var routeCount = routes.has(key) ? routes.get(key) : 0; 
-                            var minutes = Math.round((self.toDate(bus.time) - (new Date())) / 60000);
-                            if(routeCount < self.config.maxCount && minutes <= self.config.maxMinutes){
-                                routeCount++;
-                                routes.set(key, routeCount);
-                                buses.push({
-                                    number: bus.line.trim(),
-                                    from: bus.name.trim(),
-                                    to: bus.destination.trim(),
-                                    time: bus.time.trim()
-                                });
-                            }
-                        }
-                    } else {
-                        console.log(self.name + ': Request error: ' + error);
-                    }
-                    requestCount--;
-                });
-            });
+            self.readBuses(payload, buses);
+            setInterval(function () {
+                self.readBuses(payload, buses);
+            }, 30000);
         }
     },
 
-	broadcastMessage: function() {
-        self = this;
-        buses.sort(function(a, b){
-            return (self.toDate(a.time) - self.toDate(b.time));
+    readBuses: function (config, buses) {
+        var self = this;
+        stops = config.stopIds;
+        stops.forEach(function (stopId) {
+            var stopBuses = [];
+            self.getAtbStopTimes(config, stopId, function (error, data) {
+                if (!error) {
+                    var routes = new Map();
+                    for (i = 0; i < data.buses.length; i++) {
+                        var bus = data.buses[i];
+                        var key = bus.line.trim() + bus.name.trim();
+                        var routeCount = routes.has(key) ? routes.get(key) : 0;
+                        var minutes = Math.round((self.toDate(bus.time) - (new Date())) / 60000);
+                        if (routeCount < config.maxCount && minutes <= config.maxMinutes) {
+                            routeCount++;
+                            routes.set(key, routeCount);
+                            stopBuses.push({
+                                number: bus.line.trim(),
+                                from: bus.name.trim(),
+                                to: bus.destination.trim(),
+                                time: bus.time.trim()
+                            });
+                        }
+                    }
+                    buses.set(stopId, stopBuses);
+                    self.broadcastMessage(buses);
+                } else {
+                    console.error(self.name + ': Request error: ' + error);
+                }
+            });
         });
-        filteredBuses = buses.filter(function(el,i,a){
-            return !self.duplicateBuses(el, a[i - 1]); // Seems that some times AtB returns duplicated buses
-        });
-		self.sendSocketNotification('BUS_DATA', filteredBuses);
     },
 
-    printBuses(label, b){
+    broadcastMessage: function (buses) {
+        self = this;
+        busArr = [];
+        buses.forEach(function (stop) {
+            stop.forEach(function(bus) {
+                busArr.push(bus);
+            });
+        });
+        busArr.sort(function (a, b) {
+            return (self.toDate(a.time) - self.toDate(b.time));
+        });
+        filteredBuses = busArr.filter(function (el, i, a) {
+            return !self.duplicateBuses(el, a[i - 1]); // Seems that some times AtB returns duplicated buses
+        });
+        self.sendSocketNotification('BUS_DATA', filteredBuses);
+    },
+
+    printBuses(label, b) {
         console.log(label);
-        for(i=0; i < b.length; i++){
+        for (i = 0; i < b.length; i++) {
             console.log('Bus ' + i + ': Line ' + b[i].number + ' from ' + b[i].from + ' to ' + b[i].to + ' time ' + b[i].time);
         }
     },
-    
-    duplicateBuses: function(a, b){
-        if(!a) return false;
-        if(!b) return false;
-        if(a.number != b.number) return false;
-        if(a.from != b.from) return false;
-        if(a.to != b.to) return false;
-        if(a.time != b.time) return false;
+
+    duplicateBuses: function (a, b) {
+        if (!a) return false;
+        if (!b) return false;
+        if (a.number != b.number) return false;
+        if (a.from != b.from) return false;
+        if (a.to != b.to) return false;
+        if (a.time != b.time) return false;
         return true;
     },
 
-    toDate: function(s){
+    toDate: function (s) {
         year = s.substring(0, 4);
         month = parseInt(s.substring(5, 7)) - 1;
         day = s.substring(8, 10);
@@ -106,22 +99,22 @@ module.exports = NodeHelper.create({
         return time;
     },
 
-    createAtbSmsXml: function(stopId){
+    createAtbSmsXml: function (config, stopId) {
         var currentTime = new Date();
         var requestTime = currentTime.getFullYear() + '-'
-                        + (((currentTime.getMonth() + 1) < 10) ? '0' : '')
-                        + (currentTime.getMonth() + 1) + '-'
-                        + ((currentTime.getDate() < 10) ? '0' : '')
-                        + currentTime.getDate() + 'T'
-                        + ((currentTime.getHours() < 10) ? '0' : '')
-                        + currentTime.getHours() + ':'
-                        + ((currentTime.getMinutes() < 10) ? '0' : '')
-                        + currentTime.getMinutes() + ':' 
-                        + ((currentTime.getSeconds() < 10) ? '0' : '')
-                        + currentTime.getSeconds() + '.'
-                        + currentTime.getMilliseconds() + 'Z';
+            + (((currentTime.getMonth() + 1) < 10) ? '0' : '')
+            + (currentTime.getMonth() + 1) + '-'
+            + ((currentTime.getDate() < 10) ? '0' : '')
+            + currentTime.getDate() + 'T'
+            + ((currentTime.getHours() < 10) ? '0' : '')
+            + currentTime.getHours() + ':'
+            + ((currentTime.getMinutes() < 10) ? '0' : '')
+            + currentTime.getMinutes() + ':'
+            + ((currentTime.getSeconds() < 10) ? '0' : '')
+            + currentTime.getSeconds() + '.'
+            + currentTime.getMilliseconds() + 'Z';
         var requestor = 'github.com/ottopaulsen/MMM-NesteBussAtB';
-        var previewInterval = 'PT' + this.config.maxMinutes + 'M';
+        var previewInterval = 'PT' + config.maxMinutes + 'M';
         var xml = `
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:siri="http://www.siri.org.uk/siri">
         <soapenv:Header/>
@@ -143,29 +136,29 @@ module.exports = NodeHelper.create({
         `;
         return xml;
     },
-    
-    getAtbStopTimes: function(stopId, handleResponse){
+
+    getAtbStopTimes: function (config, stopId, handleResponse) {
         var self = this;
         request({
             headers: {
-              'Content-Type': 'text/xml; charset=utf-8',
-              'SoapAction': 'GetStopMonitoring'
+                'Content-Type': 'text/xml; charset=utf-8',
+                'SoapAction': 'GetStopMonitoring'
             },
             uri: 'http://st.atb.no:90/SMWS/SMService.svc?WSDL',
-            body: self.createAtbSmsXml(stopId),
+            body: self.createAtbSmsXml(config, stopId),
             method: 'POST'
-        }, function(err, res, body){
+        }, function (err, res, body) {
             result = {
                 timestamp: '',
                 buses: []
             }
-            if(err){
-                console.log('Error ' + err);
+            if (err) {
+                console.error('Error ' + err);
             } else {
-                if(res.statusCode == 200){
-                    xmlResult = convert.xml2js(body, {compact: true, spaces: 3});
+                if (res.statusCode == 200) {
+                    xmlResult = convert.xml2js(body, { compact: true, spaces: 3 });
                     //console.log(convert.xml2json(body, {compact: true, spaces: 3}));
-    
+
                     xmlBody = xmlResult['s:Envelope']['s:Body'];
                     getStopMonitoringResponse = xmlBody['GetStopMonitoringResponse'];
                     serviceDeliveryInfo = getStopMonitoringResponse['ServiceDeliveryInfo'];
@@ -173,11 +166,11 @@ module.exports = NodeHelper.create({
                     answer = getStopMonitoringResponse['Answer'];
                     stopMonitoringDelivery = answer['StopMonitoringDelivery'];
                     monitoredStopVisit = stopMonitoringDelivery['MonitoredStopVisit'];
-                    if(monitoredStopVisit){
+                    if (monitoredStopVisit) {
                         result.timestamp = responseTimestamp;
-                        for(i=0; i < monitoredStopVisit.length; i++){
+                        for (i = 0; i < monitoredStopVisit.length; i++) {
                             monitored = monitoredStopVisit[i]['MonitoredVehicleJourney']['Monitored']['_text'];
-                            if(monitored == 'true'){
+                            if (monitored == 'true') {
                                 time = monitoredStopVisit[i]['MonitoredVehicleJourney']['MonitoredCall']['ExpectedDepartureTime']['_text'];
                             } else {
                                 time = monitoredStopVisit[i]['MonitoredVehicleJourney']['MonitoredCall']['AimedDepartureTime']['_text'];
@@ -197,5 +190,5 @@ module.exports = NodeHelper.create({
             handleResponse(err, result);
         });
     }
-        
+
 });
